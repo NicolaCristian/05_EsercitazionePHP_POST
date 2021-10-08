a458afb8efc9e2110cad5999a188343efa7bab16
Nicola Cristian <nicola.cristian@osdbsesto>
2021-10-08 15:06:31 +0200

    init repo

---
+++ ricevoPost.php
@@ -0,0 +1,22 @@
+<html>
+
+<body>
+
+<h1>Prova parametri passati in post</h1>
+
+<?php
+
+//recupero valore parametro nome
+$nome_utente= $_POST['nome'];
+$email= $_POST['email'];
+
+echo "<p>Ciao". $nome_utente. "</p> ";
+echo "<p>la tua mail è". $email. "</p>";
+?>
+
+
+
+</body>
+
+
+</html>
